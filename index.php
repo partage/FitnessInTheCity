889fc908623902ef9977e8455ea90eabf78e5be7
Azaelya <djulien83@free.fr>
2013-02-26 16:02:41 +0100

    premier commit

---
+++ index.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<link rel="stylesheet" href="style.css" />
+        <script type="text/javascript" src="script_deroulant.js"></script>
+	<title>Document sans titre</title>
+</head>
+<body>
+	<div id ="bloc_page">
+            <header>
+                <div id = "titre_principal">
+                <img src="images/lp.jpg" alt="Fitness in the City" id="logo" />
+                                <div>
+
+                <h1>Fitness in the City</h1>
+                <nav>
+                    <ul id="Menu">
+                        <li><a href="#">Accueil</a></li>
+                        <li><a href="#">Nous Découvrir</a>
+                            <ul>
+                                <li><a href="#">Tarifs</a></li>
+                                <li><a href="#">Horaires</a></li>
+                                <li><a href="#">Contacts</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Nos Activités</a>
+                            <ul>
+                                <li><a href="#">Musculation</a></li>
+                                <li><a href="#">Fitness</a></li>
+                                <li><a href="#">Danse</a></li>
+                            </ul>
+                        </li>
+                        <li><a href="#">Galerie</a></li>
+                        <li><a href="#">Evènements</a></li>
+                    </ul>
+            </nav>
+            </header>
+            
+        </div>
+</body>
+</html>
